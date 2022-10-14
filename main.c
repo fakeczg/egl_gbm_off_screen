3e7e80f5f0b5b7cedd947cda1c73a79ad833fad2
fakechen <chenzigui@kylinos.cn>
2022-10-14 17:06:06 +0800

    fuck 0.002
    Signed-off-by: fakechen <chenzigui@kylinos.cn>

--- main.c
+++ main.c
@@ -10,6 +10,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <xf86drm.h>
 
 struct egl {
@@ -53,7 +54,7 @@ struct egl {
 
         // Client extensions
         bool EXT_device_query;
-        bool KHR_platform_fake;
+        bool KHR_platform_gbm;
         bool EXT_platform_device;
     } exts;
 
@@ -75,13 +76,39 @@ struct egl egl_fake;
 static bool check_egl_ext(const char *exts, const char *ext);
 static void load_egl_proc(void *proc_ptr, const char *name);
 static bool device_has_name(const drmDevice *device, const char *name);
+static bool env_parse_bool(const char *option);
 // egl debug
 static enum log_importance egl_log_importance(EGLint type);
 static const char *egl_error_str(EGLint error);
 static void egl_log(EGLenum error, const char *command, EGLint msg_type,
                     EGLLabelKHR thread, EGLLabelKHR obj, const char *msg);
 
-EGLDeviceEXT get_egl_device_from_fd(void) {
+static int open_render_node(int drm_fd) {
+    char *render_name = drmGetRenderDeviceNameFromFd(drm_fd);
+
+    if (render_name == NULL) {
+        // This can happen on split render/display platforms, fallback to
+        // primary node
+        render_name = drmGetPrimaryDeviceNameFromFd(drm_fd);
+        if (render_name == NULL) {
+            fake_log(ERROR, "drmGetPrimaryDeviceNameFromFd failed");
+            return -1;
+        }
+        fake_log(DEBUG,
+                 "DRM device '%s' has no render node, "
+                 "falling back to primary node",
+                 render_name);
+    }
+
+    int render_fd = open(render_name, O_RDWR | O_CLOEXEC);
+    if (render_fd < 0) {
+        fake_log(ERROR, "Failed to open DRM node '%s'", render_name);
+    }
+    free(render_name);
+    return render_fd;
+}
+
+EGLDeviceEXT get_egl_device_from_fd(int fd) {
     if (egl_fake.procs.eglQueryDevicesEXT == NULL) {
         fake_log(DEBUG, "EGL_EXT_device_enumeration not supported");
         return EGL_NO_DEVICE_EXT;
@@ -108,7 +135,7 @@ EGLDeviceEXT get_egl_device_from_fd(void) {
     }
 
     drmDevice *device = NULL;
-    int ret = drmGetDevice(egl_fake.card_fd, &device);
+    int ret = drmGetDevice(fd, &device);
     if (ret < 0) {
         fake_log(ERROR, "Failed to get DRM device: %s", strerror(-ret));
         return EGL_NO_DEVICE_EXT;
@@ -138,6 +165,7 @@ EGLDeviceEXT get_egl_device_from_fd(void) {
 
 static bool egl_init_display(EGLDisplay display) {
     egl_fake.display = display;
+
     EGLint major, minor;
     if (eglInitialize(egl_fake.display, &major, &minor) == EGL_FALSE) {
         fake_log(ERROR, "Failed to initialize EGL");
@@ -179,12 +207,26 @@ static bool egl_init_display(EGLDisplay display) {
             return false;
         }
         egl_fake.device = (EGLDeviceEXT)device_attrib;
+
         device_exts_str = egl_fake.procs.eglQueryDeviceStringEXT(
             egl_fake.device, EGL_EXTENSIONS);
         if (device_exts_str == NULL) {
             fake_log(ERROR, "eglQueryDeviceStringEXT(EGL_EXTENSIONS) failed");
             return false;
         }
+
+        if (check_egl_ext(device_exts_str, "EGL_MESA_device_software")) {
+            if (env_parse_bool("EGL_RENDERER_ALLOW_SOFTWARE")) {
+                fake_log(INFO, "Using software rendering");
+            } else {
+                fake_log(ERROR,
+                         "Software rendering detected, please use "
+                         "the WLR_RENDERER_ALLOW_SOFTWARE environment variable "
+                         "to proceed");
+                return false;
+            }
+        }
+
 #ifdef EGL_DRIVER_NAME_EXT
         if (check_egl_ext(device_exts_str, "EGL_EXT_device_persistent_id")) {
             driver_name = egl_fake.procs.eglQueryDeviceStringEXT(
@@ -197,8 +239,23 @@ static bool egl_init_display(EGLDisplay display) {
             check_egl_ext(device_exts_str, "EGL_EXT_device_drm_render_node");
     }
 
+    if (!check_egl_ext(display_exts_str, "EGL_KHR_no_config_context") &&
+        !check_egl_ext(display_exts_str, "EGL_MESA_configless_context")) {
+        fake_log(ERROR, "EGL_KHR_no_config_context or "
+                        "EGL_MESA_configless_context not supported");
+        return false;
+    }
+
+    if (!check_egl_ext(display_exts_str, "EGL_KHR_surfaceless_context")) {
+        fake_log(ERROR, "EGL_KHR_surfaceless_context not supported");
+        return false;
+    }
+
+    egl_fake.exts.IMG_context_priority =
+        check_egl_ext(display_exts_str, "EGL_IMG_context_priority");
+
     fake_log(INFO, "Using EGL %d.%d", (int)major, (int)minor);
-    fake_log(INFO, "Supported EGL client extensions:\n %s", display_exts_str);
+    fake_log(INFO, "Supported EGL display extensions:\n %s", display_exts_str);
     if (device_exts_str != NULL) {
         fake_log(INFO, "Supported EGL device extensions: %s", device_exts_str);
     }
@@ -207,6 +264,8 @@ static bool egl_init_display(EGLDisplay display) {
     if (driver_name != NULL) {
         fake_log(INFO, "EGL driver name: %s", driver_name);
     }
+
+    // 	init_dmabuf_formats(egl);
 
     return true;
 }
@@ -222,29 +281,105 @@ static bool egl_init(EGLenum platform, void *remote_display) {
         eglTerminate(display);
         return false;
     }
+
+    size_t atti = 0;
+    EGLint attribs[5];
+    attribs[atti++] = EGL_CONTEXT_CLIENT_VERSION;
+    attribs[atti++] = 2;
+
+    // Request a high priority context if possible
+    // TODO: only do this if we're running as the DRM master
+    bool request_high_priority = egl_fake.exts.IMG_context_priority;
+
+    // Try to reschedule all of our rendering to be completed first. If it
+    // fails, it will fallback to the default priority (MEDIUM).
+    if (request_high_priority) {
+        attribs[atti++] = EGL_CONTEXT_PRIORITY_LEVEL_IMG;
+        attribs[atti++] = EGL_CONTEXT_PRIORITY_HIGH_IMG;
+    }
+
+    attribs[atti++] = EGL_NONE;
+    assert(atti <= sizeof(attribs) / sizeof(attribs[0]));
+
+    egl_fake.context = eglCreateContext(egl_fake.display, EGL_NO_CONFIG_KHR,
+                                        EGL_NO_CONTEXT, attribs);
+    if (egl_fake.context == EGL_NO_CONTEXT) {
+        fake_log(ERROR, "Failed to create EGL context");
+        return false;
+    }
+
+    if (request_high_priority) {
+        EGLint priority = EGL_CONTEXT_PRIORITY_MEDIUM_IMG;
+        eglQueryContext(egl_fake.display, egl_fake.context,
+                        EGL_CONTEXT_PRIORITY_LEVEL_IMG, &priority);
+        if (priority != EGL_CONTEXT_PRIORITY_HIGH_IMG) {
+            fake_log(INFO, "Failed to obtain a high priority context");
+        } else {
+            fake_log(DEBUG, "Obtained high priority context");
+        }
+    }
+
     return true;
 }
 
 bool init_egl() {
 
-    // create device
-    // way1
+    // create egl device
+    egl_fake.exts.EXT_platform_device = false;
     if (egl_fake.exts.EXT_platform_device) {
         /*
          * Search for the EGL device matching the DRM fd using the
          * EXT_device_enumeration extension.
          */
-        EGLDeviceEXT egl_device = get_egl_device_from_fd();
+        EGLDeviceEXT egl_device = get_egl_device_from_fd(egl_fake.card_fd);
         if (egl_device != EGL_NO_DEVICE_EXT) {
             if (egl_init(EGL_PLATFORM_DEVICE_EXT, egl_device)) {
                 fake_log(DEBUG, "Using EGL_PLATFORM_DEVICE_EXT");
                 return true;
             }
+            goto error;
         }
 
     } else {
         fake_log(DEBUG, "EXT_platform_device not supported");
     }
+
+    if (egl_fake.exts.KHR_platform_gbm) {
+        int gbm_fd = open_render_node(egl_fake.card_fd);
+        if (gbm_fd < 0) {
+            fake_log(ERROR, "Failed to open DRM render node");
+            goto error;
+        }
+
+        egl_fake.gbm_device = gbm_create_device(gbm_fd);
+        if (!egl_fake.gbm_device) {
+            close(gbm_fd);
+            fake_log(ERROR, "Failed to create GBM device");
+            goto error;
+        }
+
+        // 这里注意，后面需要有一些显卡需要用card节点创建
+        // 比如Mali-G76是要用car0来创建gbm_device，才能拿到EGL display的
+        // 后面在修改代码；
+        if (egl_init(EGL_PLATFORM_GBM_KHR, egl_fake.gbm_device)) {
+            fake_log(DEBUG, "Using EGL_PLATFORM_GBM_KHR");
+            goto error;
+        }
+
+        gbm_device_destroy(egl_fake.gbm_device);
+        close(gbm_fd);
+    } else {
+        fake_log(DEBUG, "KHR_platform_gbm not supported");
+    }
+
+error:
+    fake_log(ERROR, "Failed to initialize EGL context");
+    if (egl_fake.display) {
+        eglMakeCurrent(egl_fake.display, EGL_NO_SURFACE, EGL_NO_SURFACE,
+                       EGL_NO_CONTEXT);
+        eglTerminate(egl_fake.display);
+    }
+    eglReleaseThread();
 
     return false;
 }
@@ -271,7 +406,7 @@ bool init_gbm(const char *path) {
     return true;
 }
 
-void check_egl() {
+bool check_egl() {
     const char *client_exts_str =
         eglQueryString(EGL_NO_DISPLAY, EGL_EXTENSIONS);
     if (client_exts_str == NULL) {
@@ -280,19 +415,21 @@ void check_egl() {
         } else {
             fake_log(ERROR, "Failed to query EGL client extensions");
         }
+        return false;
     }
 
     fake_log(INFO, "Supported EGL client extensions:\n %s", client_exts_str);
 
     if (!check_egl_ext(client_exts_str, "EGL_EXT_platform_base")) {
         fake_log(ERROR, " EGL_EXT_platform_base not supported");
+        return false;
     }
 
     load_egl_proc(&egl_fake.procs.eglGetPlatformDisplayEXT,
                   "eglGetPlatformDisplayEXT");
 
-    egl_fake.exts.KHR_platform_fake =
-        check_egl_ext(client_exts_str, "EGL_KHR_platform_fake");
+    egl_fake.exts.KHR_platform_gbm =
+        check_egl_ext(client_exts_str, "EGL_KHR_platform_gbm");
 
     egl_fake.exts.EXT_platform_device =
         check_egl_ext(client_exts_str, "EGL_EXT_platform_device");
@@ -329,7 +466,12 @@ void check_egl() {
         egl_fake.procs.eglDebugMessageControlKHR(egl_log, debug_attribs);
     }
 
-    assert(EGL_FALSE != eglBindAPI(EGL_OPENGL_ES_API));
+    if (EGL_FALSE == eglBindAPI(EGL_OPENGL_ES_API)) {
+        fake_log(ERROR, "Failed to bind to the OpenGL ES API");
+        return false;
+    }
+
+    return true;
 }
 
 bool init_kms(const char *path) {
@@ -347,7 +489,11 @@ int main(int argc, char **argv) {
 
     init_kms("/dev/dri/card0");
 
-    check_egl();
+    if (!check_egl()) {
+        fake_log(ERROR, " The current device egl cannot meet the operating "
+                        "conditions of wlroots!!!");
+    }
+    fake_log(INFO, " The device can use egl init...");
 
     // init_gbm("/dev/dri/renderD128");
     // init_fake("/dev/dri/card0");
@@ -453,4 +599,20 @@ static void egl_log(EGLenum error, const char *command, EGLint msg_type,
     _debug_log(egl_log_importance(msg_type),
                "[EGL] command: %s, error: %s (0x%x), message: \"%s\"", command,
                egl_error_str(error), error, msg);
+}
+
+static bool env_parse_bool(const char *option) {
+    const char *env = getenv(option);
+    if (env) {
+        fake_log(INFO, "Loading %s option: %s", option, env);
+    }
+
+    if (!env || strcmp(env, "0") == 0) {
+        return false;
+    } else if (strcmp(env, "1") == 0) {
+        return true;
+    }
+
+    fake_log(ERROR, "Unknown %s option: %s", option, env);
+    return false;
 }
